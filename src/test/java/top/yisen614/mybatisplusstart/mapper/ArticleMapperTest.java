cc4e08bf4bd9ba939d4ec76c08862f8475aa2840
yisen614 <yisen614@gmail.com>
2018-10-16 23:04:47 +0800

    update

--- src/test/java/top/yisen614/mybatisplusstart/mapper/ArticleMapperTest.java
+++ src/test/java/top/yisen614/mybatisplusstart/mapper/ArticleMapperTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import top.yisen614.mybatisplusstart.entity.Article;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,43 +17,43 @@ import java.util.UUID;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ArticleMapperTest {
-	@Autowired
-	private ArticleMapper articleMapper;
+    @Autowired
+    private ArticleMapper articleMapper;
 
-	@Test
-	public void insert() {
-		for (int i = 0; i < 10; i++) {
-			Article article = new Article();
-			article.setName("article:" + i);
-			article.setPubDate(new Date());
-			article.setPkId(UUID.randomUUID().toString());
-			articleMapper.insert(article);
-		}
-	}
+    @Test
+    public void insert() {
+        for (int i = 0; i < 10; i++) {
+            Article article = new Article();
+            article.setName("article:" + i);
+            article.setPubDate(new Date());
+            article.setPkId(UUID.randomUUID().toString());
+            articleMapper.insert(article);
+        }
+    }
 
-	@Test
-	public void delete() {
-		int n = articleMapper.delete(new QueryWrapper<Article>().eq("pk_id", "1b4fff9c-5909-4bc4-9ead-3f415145caf7"));
-		Assert.assertEquals(1, n);
-	}
+    @Test
+    public void delete() {
+        int n = articleMapper.delete(new QueryWrapper<Article>().eq("pk_id", "1b4fff9c-5909-4bc4-9ead-3f415145caf7"));
+        Assert.assertEquals(1, n);
+    }
 
-	@Test
-	public void deleteById() {
-		int n = articleMapper.deleteById("25d37b23-5743-4019-b793-ad45323f7b0a");
-	}
+    @Test
+    public void deleteById() {
+        int n = articleMapper.deleteById("25d37b23-5743-4019-b793-ad45323f7b0a");
+    }
 
-	@Test
-	public void deleteMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("pk_id", "5da834bc-f7a4-4a5a-9ec4-1e999f83a34e");
-		map.put("name", "article:8");
-		int n = articleMapper.deleteByMap(map);
-		Assert.assertEquals(1, n);
-	}
+    @Test
+    public void deleteMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("pk_id", "fdsafasfdsa");
+        map.put("name", "dsfasf");
+        int n = articleMapper.deleteByMap(map);
+        Assert.assertEquals(1, n);
+    }
 
-	@Test
-	public void insertOne() {
-		int n = articleMapper.insertArticle(new Date());
-		Assert.assertEquals(1, n);
-	}
+    @Test
+    public void insertOne() {
+        int n = articleMapper.insertArticle(new Date());
+        Assert.assertEquals(1, n);
+    }
 }
--- src/test/java/top/yisen614/mybatisplusstart/mapper/UserMapperTest.java
+++ src/test/java/top/yisen614/mybatisplusstart/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package top.yisen614.mybatisplusstart.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import top.yisen614.mybatisplusstart.entity.User;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -25,4 +27,39 @@ public class UserMapperTest {
 			user.setIsEnabled(1);
 		}
 	}
+
+	@Test
+	public void select() {
+        System.out.println("----- 普通查询 ------");
+        List<User> users = userMapper.selectList(new QueryWrapper<User>().eq("account", "account5"));
+		List<User> lambdaUsers = userMapper.selectList(new QueryWrapper<User>().lambda().eq(User::getAccount, "account5"));
+		lambdaUsers.forEach(System.out::println);
+		users.forEach(System.out::println);
+        System.out.println("----- 带子查询(sql注入) ------");
+        List<User> users1 = userMapper.selectList(new QueryWrapper<User>()
+                .inSql("account", "select account from t_user where pk_id = '06299f1069de4f98aa640f08ef598e35'"));
+        List<User> lambdaUsers2 = userMapper.selectList(new QueryWrapper<User>().lambda()
+                .inSql(User::getAccount, "select account from t_user where pk_id = '06299f1069de4f98aa640f08ef598e35'"));
+        users1.forEach(System.out::println);
+        lambdaUsers2.forEach(System.out::println);
+
+        System.out.println("----- 带嵌套查询 ------");
+        List<User> users2 = userMapper.selectList(new QueryWrapper<User>()
+                .nested(i -> i.eq("account", "account5").or().eq("pk_id", "06299f1069de4f98aa640f08ef598e35"))
+                .and(i -> i.ge("password", "password5")));
+        List<User> lambdaUsers3 = userMapper.selectList(new QueryWrapper<User>().lambda()
+                .nested(i -> i.eq(User::getAccount, 2L).or().eq(User::getAccount, "account5"))
+                .and(i -> i.ge(User::getPassword, "password5")));
+
+        users2.forEach(System.out::println);
+        lambdaUsers3.forEach(System.out::println);
+
+        System.out.println("----- 自定义(sql注入) ------");
+        List<User> users3 = userMapper.selectList(new QueryWrapper<User>()
+                .apply("account = 'account5'"));
+
+        users3.forEach(System.out::println);
+
+	}
+
 }
